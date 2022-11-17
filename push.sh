2b579dcd1ea13a9ff3192c2c8d7abd12aa7b88d1
AngeloEndyy <angelo.chukwu.245408@unn.edu.ng>
2022-11-17 14:05:15 -0800

    [Feature]: Twitter responsive timeline

---
+++ push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "# Twitter-comment-section" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AngeloEndyy/Twitter-comment-section.git
+git push -u origin main
